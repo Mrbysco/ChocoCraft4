118c02ce21effec01a976d9d87b58825334b3d73
Mrbysco <Mrbysco@users.noreply.github.com>
2021-10-23 21:35:50 +0200

    Let's use `isCrouching` and create the chocobo using the EntityType's create method

--- src/main/java/net/chococraft/common/items/ChocoboSpawnEggItem.java
+++ src/main/java/net/chococraft/common/items/ChocoboSpawnEggItem.java
@@ -3,6 +3,7 @@ package net.chococraft.common.items;
 import net.chococraft.Chococraft;
 import net.chococraft.common.entities.ChocoboEntity;
 import net.chococraft.common.entities.properties.ChocoboColor;
+import net.chococraft.common.init.ModEntities;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.SpawnReason;
@@ -32,25 +33,22 @@ public class ChocoboSpawnEggItem extends Item {
             return ActionResultType.SUCCESS;
         }
 
-        Entity entity = ForgeRegistries.ENTITIES.getValue(new ResourceLocation(Chococraft.MODID, "chocobo")).create(worldIn);
-        if (entity instanceof ChocoboEntity) {
-            final ChocoboEntity chocobo = (ChocoboEntity) entity;
+        final ChocoboEntity chocobo = ModEntities.CHOCOBO.get().create(worldIn);
+        if (chocobo != null) {
             final BlockPos pos = context.getClickedPos();
-
             final PlayerEntity player = context.getPlayer();
             if (player != null) {
-                player.isCrouching();
-                if (player.isShiftKeyDown()) {
+                if (player.isCrouching()) {
                     chocobo.setAge(-24000);
                 }
             }
 
-            entity.moveTo(pos.getX() + .5, pos.getY() + 0.5F, pos.getZ() + .5, MathHelper.wrapDegrees(worldIn.random.nextFloat() * 360.0F), 0.0F);
+            chocobo.moveTo(pos.getX() + .5, pos.getY() + 0.5F, pos.getZ() + .5, MathHelper.wrapDegrees(worldIn.random.nextFloat() * 360.0F), 0.0F);
             chocobo.yHeadRot = chocobo.yRot;
             chocobo.yBodyRot = chocobo.yRot;
             chocobo.setChocoboColor(color);
             chocobo.finalizeSpawn((ServerWorld)worldIn, worldIn.getCurrentDifficultyAt(chocobo.blockPosition()), SpawnReason.SPAWN_EGG, null, null);
-            worldIn.addFreshEntity(entity);
+            worldIn.addFreshEntity(chocobo);
             chocobo.playAmbientSound();
         }
 
