ac3babf0ebd16ae5d10819c6270a965bf712966a
Mrbysco <Mrbysco@users.noreply.github.com>
2021-10-24 23:49:43 +0200

    Fix the armor name
    Change the ai behavior a slight bit to match old chococraft

--- src/main/java/net/chococraft/common/entities/ChocoboEntity.java
+++ src/main/java/net/chococraft/common/entities/ChocoboEntity.java
@@ -155,7 +155,6 @@ public class ChocoboEntity extends TameableEntity {
         this.goalSelector.addGoal(2, new ChocoboMateGoal(this, 1.0D));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 0.6D));
         this.goalSelector.addGoal(4, new TemptGoal(this, 1.2D, false, Ingredient.of(ModRegistry.GYSAHL_GREEN.get())));
-        this.goalSelector.addGoal(5, chocoboAvoidPlayerGoal);
         this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
         this.goalSelector.addGoal(1, new SwimGoal(this));
@@ -834,6 +833,10 @@ public class ChocoboEntity extends TameableEntity {
     @Override
     protected void reassessTameGoals() {
         super.reassessTameGoals();
-        goalSelector.removeGoal(chocoboAvoidPlayerGoal);
+        if(isTame()) {
+            goalSelector.removeGoal(chocoboAvoidPlayerGoal);
+        } else {
+            goalSelector.addGoal(5, chocoboAvoidPlayerGoal);
+        }
     }
 }
