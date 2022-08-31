0188cd83acd2fef88dea8a45124c57b15fb14827
Mrbysco <Mrbysco@users.noreply.github.com>
2022-08-31 21:42:56 +0200

    Reorder AI Goals

--- src/main/java/net/chococraft/common/entities/Chocobo.java
+++ src/main/java/net/chococraft/common/entities/Chocobo.java
@@ -146,12 +146,12 @@ public class Chocobo extends TamableAnimal {
 
 	@Override
 	protected void registerGoals() {
-		this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-		this.goalSelector.addGoal(1, new ChocoboFollowOwnerGoal(this, 1.0D, 5.0F, 5.0F));
-		this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-		this.goalSelector.addGoal(1, new LookAtPlayerGoal(this, Player.class, 5.0F));
-		this.goalSelector.addGoal(1, new RandomLookAroundGoal(this));
-		this.goalSelector.addGoal(1, new FloatGoal(this));
+		this.goalSelector.addGoal(0, new FloatGoal(this));
+		this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
+		this.goalSelector.addGoal(4, new ChocoboFollowOwnerGoal(this, 1.0D, 5.0F, 5.0F));
+		this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+		this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 5.0F));
+		this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
 	}
 
 	private final ChocoboFollowOwnerGoal follow = new ChocoboFollowOwnerGoal(this, 2.0D, 3.0F, 10.0F);
@@ -457,7 +457,6 @@ public class Chocobo extends TamableAnimal {
 
 		if (this.getCommandSenderWorld().isClientSide) {
 			// Wing rotations, control packet, client side
-			// Client side
 			this.destPos += (double) (this.onGround ? -1 : 4) * 0.3D;
 			this.destPos = Mth.clamp(destPos, 0f, 1f);
 
@@ -506,23 +505,25 @@ public class Chocobo extends TamableAnimal {
 	public InteractionResult mobInteract(Player player, InteractionHand hand) {
 		ItemStack heldItemStack = player.getItemInHand(hand);
 		if (this.isFood(heldItemStack)) {
-			int i = this.getAge();
+			boolean fedCake = heldItemStack.getItem() == ModRegistry.GYSAHL_CAKE.get();
 			//Allow the Chocobo to breed by feeding Loverly or Gold Gysahl
-			if (!this.level.isClientSide && i == 0 && this.canFallInLove()) {
-				if (heldItemStack.is(ModRegistry.GOLD_GYSAHL.get())) {
-					//If fed a Gold Gysahl set the "fedGoldGysahl" flag to true
-					this.setFedGoldGysahl(true);
+			if (this.isBaby()) {
+				if (fedCake) {
+					this.usePlayerItem(player, hand, heldItemStack);
+					this.ageBoundaryReached();
+					return InteractionResult.sidedSuccess(this.level.isClientSide);
 				}
-				this.usePlayerItem(player, hand, player.getInventory().getSelected());
-				this.setInLove(player);
-				return InteractionResult.SUCCESS;
-			}
-
-			//Instantly age the Chocobo if fed Gysahl Cake
-			if (this.isBaby() && heldItemStack.is(ModRegistry.GYSAHL_CAKE.get())) {
-				this.usePlayerItem(player, hand, heldItemStack);
-				this.ageBoundaryReached();
-				return InteractionResult.sidedSuccess(this.level.isClientSide);
+			} else {
+				int i = this.getAge();
+				if (!this.level.isClientSide && i == 0 && this.canFallInLove() && !fedCake) {
+					if (heldItemStack.getItem() == ModRegistry.GOLD_GYSAHL.get()) {
+						//If fed a Gold Gysahl set the "fedGoldGysahl" flag to true
+						this.setFedGoldGysahl(true);
+					}
+					this.usePlayerItem(player, hand, heldItemStack);
+					this.setInLove(player);
+					return InteractionResult.SUCCESS;
+				}
 			}
 
 			if (this.level.isClientSide) {
