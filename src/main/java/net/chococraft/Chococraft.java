d4c896b1f0cce6c41b82c955cd46a93880e33f1d
Mrbysco <Mrbysco@users.noreply.github.com>
2023-01-09 21:15:32 +0100

    Redo the regular chocopedia

--- src/main/java/net/chococraft/Chococraft.java
+++ src/main/java/net/chococraft/Chococraft.java
@@ -70,6 +70,7 @@ public class Chococraft {
 		event.enqueueWork(() -> {
 			registerCompostables();
 		});
+
 	}
 
 	public static void registerCompostables() {
--- src/main/java/net/chococraft/client/gui/ChocoboBookScreen.java
+++ src/main/java/net/chococraft/client/gui/ChocoboBookScreen.java
@@ -4,25 +4,42 @@ package net.chococraft.client.gui;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.chococraft.Chococraft;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.PageButton;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.ForgeI18n;
 
 
 public class ChocoboBookScreen extends Screen {
 	private final static ResourceLocation TEXTURE = new ResourceLocation(Chococraft.MODID, "textures/gui/chocobo_book.png");
+	private final Component bookTitle = Component.literal("The Chocopedia").withStyle(ChatFormatting.GOLD);
+	private final Component bookAuthor = Component.literal("by Clienthax");
 
-	private int xSize = 130;
-	private int ySize = 185;
-	private int currentpage = 1;
+	private int xSize = 192;
+	private int ySize = 192;
+	private int currentPage = 0;
+	private final int pageCount;
 	private int guiLeft;
 	private int guiTop;
+	private Component pageMsg = CommonComponents.EMPTY;
 
 	public ChocoboBookScreen() {
 		super(Component.empty());
+
+		int maxPages = 1;
+		for (int i = 1; i < 128; i++) {
+			String unlocalized = "gui.chocobook.page" + i;
+			if (ForgeI18n.getPattern(unlocalized).equals(unlocalized)) {
+				maxPages = i - 1;
+				break;
+			}
+		}
+		this.pageCount = maxPages;
 	}
 
 	public static void openScreen() {
@@ -34,16 +51,17 @@ public class ChocoboBookScreen extends Screen {
 		this.guiLeft = (this.width - this.xSize) / 2;
 		this.guiTop = (this.height - this.ySize) / 2;
 
-		this.addRenderableWidget(new Button(this.guiLeft, this.guiTop + 165, 20, 20, Component.empty(), (button) ->
-				this.currentpage = (this.currentpage <= 1 ? 7 : this.currentpage - 1)));
-		this.addRenderableWidget(new Button((this.guiLeft + xSize) - 20, this.guiTop + 165, 20, 20, Component.empty(), (button) ->
-				this.currentpage = (this.currentpage >= 7 ? 1 : this.currentpage + 1)));
+		this.addRenderableWidget(new PageButton((this.guiLeft + xSize) - 66, this.guiTop + 158, true, (p_98297_) -> {
+			this.currentPage = (this.currentPage >= pageCount ? 0 : this.currentPage + 1);
+		}, true));
+		this.addRenderableWidget(new PageButton(this.guiLeft + 36, this.guiTop + 158, false, (p_98287_) -> {
+			this.currentPage = (this.currentPage <= 0 ? pageCount : this.currentPage - 1);
+		}, true));
 	}
 
 	@Override
 	public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
 		this.renderBackground(poseStack);
-		super.render(poseStack, mouseX, mouseY, partialTicks);
 		RenderSystem.setShader(GameRenderer::getPositionTexShader);
 		RenderSystem.setShaderTexture(0, TEXTURE);
 
@@ -52,17 +70,24 @@ public class ChocoboBookScreen extends Screen {
 
 		this.blit(poseStack, 0, 0, 0, 0, this.xSize, this.ySize);
 
-		Component name = Component.translatable("gui.chocobook.title", currentpage);
-		int nameLength = this.font.width(name);
-		this.font.drawShadow(poseStack, name, (this.xSize / 2) - (nameLength / 2), 4, -1);
+		if (this.currentPage == 0) {
+			this.font.draw(poseStack, bookTitle, (this.xSize / 2) - (this.font.width(bookTitle) / 2), 24, 0);
+			this.font.draw(poseStack, bookAuthor, (this.xSize / 2) - (this.font.width(bookAuthor) / 2), 44, 0);
+		} else {
+			if (currentPage > 1) {
+				this.pageMsg = Component.translatable("book.pageIndicator", currentPage - 1, Math.max(pageCount - 1, 1));
+				this.font.draw(poseStack, this.pageMsg, ((this.xSize / 2) - (this.font.width(bookAuthor) / 2) - 6), 14, 0);
+			}
 
-		this.renderpage();
-
+			this.renderpage();
+		}
 		poseStack.popPose();
+		super.render(poseStack, mouseX, mouseY, partialTicks);
 	}
 
 	private void renderpage() {
-		this.font.drawWordWrap(Component.translatable("gui.chocobook.page" + (currentpage)), this.guiLeft + 5, this.guiTop + 20, 120, 0);
+		int i = (this.width - this.xSize) / 2;
+		this.font.drawWordWrap(Component.translatable("gui.chocobook.page" + (currentPage)), i + 34, this.guiTop + 26, 120, 0);
 	}
 
 }
